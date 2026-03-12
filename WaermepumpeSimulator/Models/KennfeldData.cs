5e7d5232d9f68868e3a3248a13581ecab4eed954
kenan-kajkus <kenan@kajkus.eu>
2026-03-12 12:31:57 +0100

    make kennfeld saveable

---
+++ WaermepumpeSimulator/Models/KennfeldData.cs
@@ -0,0 +1,8 @@
+namespace WaermepumpeSimulator.Models;
+
+public class KennfeldData
+{
+    public string PMax { get; set; } = "";
+    public string PMin { get; set; } = "";
+    public string CopData { get; set; } = "";
+}
