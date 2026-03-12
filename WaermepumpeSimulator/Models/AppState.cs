fc0ab02c8fc351cabec78ac092c59c413cea08ef
kenan-kajkus <kenan@kajkus.eu>
2026-03-12 13:20:38 +0100

    Save and restore localization

---
+++ WaermepumpeSimulator/Models/AppState.cs
@@ -0,0 +1,14 @@
+namespace WaermepumpeSimulator.Models;
+
+public class AppState
+{
+    public SimulationParameters Params { get; set; } = new();
+    public string SelectedCity { get; set; } = "frankfurt";
+    public string SelectedYear { get; set; } = "2023";
+    public string SelectedPreset { get; set; } = "custom";
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string? GeoLocationName { get; set; }
+    public double? GeoLatitude { get; set; }
+    public double? GeoLongitude { get; set; }
+}
